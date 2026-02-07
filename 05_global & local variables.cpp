c0135ff9bc106be7abe3ece84e849f582861ac16
Om <omgurav295@gmail.com>
2026-02-07 14:32:34 +0530

    Create 05_global & local variables.cpp

---
+++ 05_global & local variables.cpp
@@ -0,0 +1,9 @@
+Day 4: Learning global and local variables in C++
+
+Today I learned about global and local variables in C++.
+
+A local variable is declared inside a function and can only be used within that function. It exists only while the function is running. This helps keep code organized and prevents accidental changes from other parts of the program.
+
+A global variable is declared outside all functions and can be accessed from anywhere in the program. It is useful when multiple functions need to share the same data.
+
+I practiced writing simple programs to understand the difference between global and local scope. This lesson helped me understand how variables behave in different parts of a program.
